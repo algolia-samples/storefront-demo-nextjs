e40fbb7d7455965e7b0ba33acb3c306bcee51090
Dhaya <154633+dhayab@users.noreply.github.com>
2022-08-24 14:10:54 +0200

    feat: wrap pages in shared layout

---
+++ mock.ts
@@ -0,0 +1,285 @@
+/* Layout */
+
+export const currencies = ['CAD', 'USD', 'AUD', 'EUR', 'GBP'];
+
+export const navigation = {
+  categories: [
+    {
+      name: 'Women',
+      featured: [
+        {
+          name: 'New Arrivals',
+          href: '#',
+          imageSrc:
+            'https://tailwindui.com/img/ecommerce-images/mega-menu-category-01.jpg',
+          imageAlt:
+            'Models sitting back to back, wearing Basic Tee in black and bone.',
+        },
+        {
+          name: 'Basic Tees',
+          href: '#',
+          imageSrc:
+            'https://tailwindui.com/img/ecommerce-images/mega-menu-category-02.jpg',
+          imageAlt:
+            'Close up of Basic Tee fall bundle with off-white, ochre, olive, and black tees.',
+        },
+        {
+          name: 'Accessories',
+          href: '#',
+          imageSrc:
+            'https://tailwindui.com/img/ecommerce-images/mega-menu-category-03.jpg',
+          imageAlt:
+            'Model wearing minimalist watch with black wristband and white watch face.',
+        },
+        {
+          name: 'Carry',
+          href: '#',
+          imageSrc:
+            'https://tailwindui.com/img/ecommerce-images/mega-menu-category-04.jpg',
+          imageAlt:
+            'Model opening tan leather long wallet with credit card pockets and cash pouch.',
+        },
+      ],
+    },
+    {
+      name: 'Men',
+      featured: [
+        {
+          name: 'New Arrivals',
+          href: '#',
+          imageSrc:
+            'https://tailwindui.com/img/ecommerce-images/mega-menu-01-men-category-01.jpg',
+          imageAlt:
+            'Hats and sweaters on wood shelves next to various colors of t-shirts on hangers.',
+        },
+        {
+          name: 'Basic Tees',
+          href: '#',
+          imageSrc:
+            'https://tailwindui.com/img/ecommerce-images/mega-menu-01-men-category-02.jpg',
+          imageAlt: 'Model wearing light heather gray t-shirt.',
+        },
+        {
+          name: 'Accessories',
+          href: '#',
+          imageSrc:
+            'https://tailwindui.com/img/ecommerce-images/mega-menu-01-men-category-03.jpg',
+          imageAlt:
+            'Grey 6-panel baseball hat with black brim, black mountain graphic on front, and light heather gray body.',
+        },
+        {
+          name: 'Carry',
+          href: '#',
+          imageSrc:
+            'https://tailwindui.com/img/ecommerce-images/mega-menu-01-men-category-04.jpg',
+          imageAlt:
+            'Model putting folded cash into slim card holder olive leather wallet with hand stitching.',
+        },
+      ],
+    },
+  ],
+  pages: [
+    { name: 'Company', href: '#' },
+    { name: 'Stores', href: '#' },
+  ],
+};
+
+export const footerNavigation = {
+  products: [
+    { name: 'Bags', href: '#' },
+    { name: 'Tees', href: '#' },
+    { name: 'Objects', href: '#' },
+    { name: 'Home Goods', href: '#' },
+    { name: 'Accessories', href: '#' },
+  ],
+  company: [
+    { name: 'Who we are', href: '#' },
+    { name: 'Sustainability', href: '#' },
+    { name: 'Press', href: '#' },
+    { name: 'Careers', href: '#' },
+    { name: 'Terms & Conditions', href: '#' },
+    { name: 'Privacy', href: '#' },
+  ],
+  customerService: [
+    { name: 'Contact', href: '#' },
+    { name: 'Shipping', href: '#' },
+    { name: 'Returns', href: '#' },
+    { name: 'Warranty', href: '#' },
+    { name: 'Secure Payments', href: '#' },
+    { name: 'FAQ', href: '#' },
+    { name: 'Find a store', href: '#' },
+  ],
+};
+
+export const perks = [
+  {
+    name: 'Free returns',
+    imageUrl:
+      'https://tailwindui.com/img/ecommerce/icons/icon-returns-light.svg',
+    description:
+      'Not what you expected? Place it back in the parcel and attach the pre-paid postage stamp.',
+  },
+  {
+    name: 'Same day delivery',
+    imageUrl:
+      'https://tailwindui.com/img/ecommerce/icons/icon-calendar-light.svg',
+    description:
+      'We offer a delivery service that has never been done before. Checkout today and receive your products within hours.',
+  },
+  {
+    name: 'All year discount',
+    imageUrl:
+      'https://tailwindui.com/img/ecommerce/icons/icon-gift-card-light.svg',
+    description:
+      'Looking for a deal? You can use the code "ALLYEAR" at checkout and get money off all year round.',
+  },
+  {
+    name: 'For the planet',
+    imageUrl:
+      'https://tailwindui.com/img/ecommerce/icons/icon-planet-light.svg',
+    description:
+      'We’ve pledged 1% of sales to the preservation and restoration of the natural environment.',
+  },
+];
+
+/* Home */
+
+export const collections = [
+  {
+    name: "Women's",
+    href: '#',
+    imageSrc:
+      'https://tailwindui.com/img/ecommerce-images/home-page-04-collection-01.jpg',
+    imageAlt: 'Woman wearing a comfortable cotton t-shirt.',
+  },
+  {
+    name: "Men's",
+    href: '#',
+    imageSrc:
+      'https://tailwindui.com/img/ecommerce-images/home-page-04-collection-02.jpg',
+    imageAlt: 'Man wearing a comfortable and casual cotton t-shirt.',
+  },
+  {
+    name: 'Desk Accessories',
+    href: '#',
+    imageSrc:
+      'https://tailwindui.com/img/ecommerce-images/home-page-04-collection-03.jpg',
+    imageAlt:
+      'Person sitting at a wooden desk with paper note organizer, pencil and tablet.',
+  },
+];
+
+export const trendingProducts = [
+  {
+    id: 1,
+    name: 'Leather Long Wallet',
+    color: 'Natural',
+    price: '$75',
+    href: '#',
+    imageSrc:
+      'https://tailwindui.com/img/ecommerce-images/home-page-04-trending-product-02.jpg',
+    imageAlt: 'Hand stitched, orange leather long wallet.',
+  },
+  {
+    id: 2,
+    name: 'Leather Long Wallet',
+    color: 'Natural',
+    price: '$75',
+    href: '#',
+    imageSrc:
+      'https://tailwindui.com/img/ecommerce-images/home-page-04-trending-product-02.jpg',
+    imageAlt: 'Hand stitched, orange leather long wallet.',
+  },
+  {
+    id: 3,
+    name: 'Leather Long Wallet',
+    color: 'Natural',
+    price: '$75',
+    href: '#',
+    imageSrc:
+      'https://tailwindui.com/img/ecommerce-images/home-page-04-trending-product-02.jpg',
+    imageAlt: 'Hand stitched, orange leather long wallet.',
+  },
+  {
+    id: 4,
+    name: 'Leather Long Wallet',
+    color: 'Natural',
+    price: '$75',
+    href: '#',
+    imageSrc:
+      'https://tailwindui.com/img/ecommerce-images/home-page-04-trending-product-02.jpg',
+    imageAlt: 'Hand stitched, orange leather long wallet.',
+  },
+  // More products...
+];
+
+/** Search */
+
+export const breadcrumbs = [{ id: 1, name: 'Men', href: '#' }];
+
+export const filters = [
+  {
+    id: 'color',
+    name: 'Color',
+    options: [
+      { value: 'white', label: 'White' },
+      { value: 'beige', label: 'Beige' },
+      { value: 'blue', label: 'Blue' },
+      { value: 'brown', label: 'Brown' },
+      { value: 'green', label: 'Green' },
+      { value: 'purple', label: 'Purple' },
+    ],
+  },
+  {
+    id: 'category',
+    name: 'Category',
+    options: [
+      { value: 'new-arrivals', label: 'All New Arrivals' },
+      { value: 'tees', label: 'Tees' },
+      { value: 'crewnecks', label: 'Crewnecks' },
+      { value: 'sweatshirts', label: 'Sweatshirts' },
+      { value: 'pants-shorts', label: 'Pants & Shorts' },
+    ],
+  },
+  {
+    id: 'sizes',
+    name: 'Sizes',
+    options: [
+      { value: 'xs', label: 'XS' },
+      { value: 's', label: 'S' },
+      { value: 'm', label: 'M' },
+      { value: 'l', label: 'L' },
+      { value: 'xl', label: 'XL' },
+      { value: '2xl', label: '2XL' },
+    ],
+  },
+];
+
+export const products = [
+  {
+    id: 1,
+    name: 'Basic Tee 8-Pack',
+    href: '#',
+    price: '$256',
+    description:
+      'Get the full lineup of our Basic Tees. Have a fresh shirt all week, and an extra for laundry day.',
+    options: '8 colors',
+    imageSrc:
+      'https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-01.jpg',
+    imageAlt:
+      'Eight shirts arranged on table in black, olive, grey, blue, white, red, mustard, and green.',
+  },
+  {
+    id: 2,
+    name: 'Basic Tee',
+    href: '#',
+    price: '$32',
+    description:
+      'Look like a visionary CEO and wear the same black t-shirt every day.',
+    options: 'Black',
+    imageSrc:
+      'https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-02.jpg',
+    imageAlt: 'Front of plain black t-shirt.',
+  },
+  // More products...
+];
