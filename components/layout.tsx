a037d1f82cf5ba2be9cfe58e54790512d8ff8684
Sarah Dayan <5370675+sarahdayan@users.noreply.github.com>
2022-09-01 14:51:38 +0200

    feat: move secondary navigation to the left

--- components/layout.tsx
+++ components/layout.tsx
@@ -259,129 +259,133 @@ export default function Layout({ children }: PropsWithChildren) {
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
               <div className="h-16 flex items-center justify-between">
                 {/* Logo (lg+) */}
-                <div className="hidden lg:flex-1 lg:flex lg:items-center">
-                  <Link href="/">
-                    <a>
-                      <span className="sr-only">Workflow</span>
-                      <img
-                        className="h-8 w-auto"
-                        src="https://tailwindui.com/img/logos/workflow-mark.svg?color=indigo&shade=600"
-                        alt=""
-                      />
-                    </a>
-                  </Link>
-                </div>
+                <div className="hidden h-full lg:flex lg:items-center">
+                  <div className="hidden lg:flex-1 lg:flex lg:items-center mr-4">
+                    <Link href="/">
+                      <a>
+                        <span className="sr-only">Workflow</span>
+                        <img
+                          className="h-8 w-auto"
+                          src="https://tailwindui.com/img/logos/workflow-mark.svg?color=indigo&shade=600"
+                          alt=""
+                        />
+                      </a>
+                    </Link>
+                  </div>
 
-                <div className="hidden h-full lg:flex">
-                  {/* Flyout menus */}
-                  <Popover.Group className="px-4 bottom-0 inset-x-0">
-                    <div className="h-full flex justify-center space-x-8">
-                      {navigation.categories.map((category) => (
-                        <Popover key={category.name} className="flex">
-                          {({ open }) => (
-                            <>
-                              <div className="relative flex">
-                                <Popover.Button
-                                  className={cx(
-                                    open
-                                      ? 'text-indigo-600'
-                                      : 'text-gray-700 hover:text-gray-800',
-                                    'relative flex items-center justify-center transition-colors ease-out duration-200 text-sm font-medium'
-                                  )}
-                                >
-                                  {category.name}
-                                  <span
+                  <div className="hidden h-full lg:flex">
+                    {/* Flyout menus */}
+                    <Popover.Group className="px-4 bottom-0 inset-x-0">
+                      <div className="h-full flex justify-center space-x-8">
+                        {navigation.categories.map((category) => (
+                          <Popover key={category.name} className="flex">
+                            {({ open }) => (
+                              <>
+                                <div className="relative flex">
+                                  <Popover.Button
                                     className={cx(
-                                      open ? 'bg-indigo-600' : '',
-                                      'absolute z-20 -bottom-px inset-x-0 h-0.5 transition ease-out duration-200'
+                                      open
+                                        ? 'text-indigo-600'
+                                        : 'text-gray-700 hover:text-gray-800',
+                                      'relative flex items-center justify-center transition-colors ease-out duration-200 text-sm font-medium'
                                     )}
-                                    aria-hidden="true"
-                                  />
-                                </Popover.Button>
-                              </div>
-
-                              <Transition
-                                as={Fragment}
-                                enter="transition ease-out duration-200"
-                                enterFrom="opacity-0"
-                                enterTo="opacity-100"
-                                leave="transition ease-in duration-150"
-                                leaveFrom="opacity-100"
-                                leaveTo="opacity-0"
-                              >
-                                <Popover.Panel className="absolute z-10 top-full inset-x-0 bg-white text-sm text-gray-500">
-                                  {/* Presentational element used to render the bottom shadow, if we put the shadow on the actual panel it pokes out the top, so we use this shorter element to hide the top of the shadow */}
-                                  <div
-                                    className="absolute inset-0 top-1/2 bg-white shadow"
-                                    aria-hidden="true"
-                                  />
-                                  {/* Fake border when menu is open */}
-                                  <div
-                                    className="absolute inset-0 top-0 h-px max-w-7xl mx-auto px-8"
-                                    aria-hidden="true"
                                   >
-                                    <div
+                                    {category.name}
+                                    <span
                                       className={cx(
-                                        open ? 'bg-gray-200' : 'bg-transparent',
-                                        'w-full h-px transition-colors ease-out duration-200'
+                                        open ? 'bg-indigo-600' : '',
+                                        'absolute z-20 -bottom-px inset-x-0 h-0.5 transition ease-out duration-200'
                                       )}
+                                      aria-hidden="true"
                                     />
-                                  </div>
+                                  </Popover.Button>
+                                </div>
 
-                                  <div className="relative">
-                                    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                                      <div className="grid grid-cols-4 gap-y-10 gap-x-8 py-16">
-                                        {category.featured.map((item) => (
-                                          <div
-                                            key={item.name}
-                                            className="group relative"
-                                          >
-                                            <div className="aspect-w-1 aspect-h-1 rounded-md bg-gray-100 overflow-hidden group-hover:opacity-75">
-                                              <img
-                                                src={item.imageSrc}
-                                                alt={item.imageAlt}
-                                                className="object-center object-cover"
-                                              />
-                                            </div>
-                                            <a
-                                              href={item.href}
-                                              className="mt-4 block font-medium text-gray-900"
+                                <Transition
+                                  as={Fragment}
+                                  enter="transition ease-out duration-200"
+                                  enterFrom="opacity-0"
+                                  enterTo="opacity-100"
+                                  leave="transition ease-in duration-150"
+                                  leaveFrom="opacity-100"
+                                  leaveTo="opacity-0"
+                                >
+                                  <Popover.Panel className="absolute z-10 top-full inset-x-0 bg-white text-sm text-gray-500">
+                                    {/* Presentational element used to render the bottom shadow, if we put the shadow on the actual panel it pokes out the top, so we use this shorter element to hide the top of the shadow */}
+                                    <div
+                                      className="absolute inset-0 top-1/2 bg-white shadow"
+                                      aria-hidden="true"
+                                    />
+                                    {/* Fake border when menu is open */}
+                                    <div
+                                      className="absolute inset-0 top-0 h-px max-w-7xl mx-auto px-8"
+                                      aria-hidden="true"
+                                    >
+                                      <div
+                                        className={cx(
+                                          open
+                                            ? 'bg-gray-200'
+                                            : 'bg-transparent',
+                                          'w-full h-px transition-colors ease-out duration-200'
+                                        )}
+                                      />
+                                    </div>
+
+                                    <div className="relative">
+                                      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                                        <div className="grid grid-cols-4 gap-y-10 gap-x-8 py-16">
+                                          {category.featured.map((item) => (
+                                            <div
+                                              key={item.name}
+                                              className="group relative"
                                             >
-                                              <span
-                                                className="absolute z-10 inset-0"
+                                              <div className="aspect-w-1 aspect-h-1 rounded-md bg-gray-100 overflow-hidden group-hover:opacity-75">
+                                                <img
+                                                  src={item.imageSrc}
+                                                  alt={item.imageAlt}
+                                                  className="object-center object-cover"
+                                                />
+                                              </div>
+                                              <a
+                                                href={item.href}
+                                                className="mt-4 block font-medium text-gray-900"
+                                              >
+                                                <span
+                                                  className="absolute z-10 inset-0"
+                                                  aria-hidden="true"
+                                                />
+                                                {item.name}
+                                              </a>
+                                              <p
                                                 aria-hidden="true"
-                                              />
-                                              {item.name}
-                                            </a>
-                                            <p
-                                              aria-hidden="true"
-                                              className="mt-1"
-                                            >
-                                              Shop now
-                                            </p>
-                                          </div>
-                                        ))}
+                                                className="mt-1"
+                                              >
+                                                Shop now
+                                              </p>
+                                            </div>
+                                          ))}
+                                        </div>
                                       </div>
                                     </div>
-                                  </div>
-                                </Popover.Panel>
-                              </Transition>
-                            </>
-                          )}
-                        </Popover>
-                      ))}
+                                  </Popover.Panel>
+                                </Transition>
+                              </>
+                            )}
+                          </Popover>
+                        ))}
 
-                      {navigation.pages.map((page) => (
-                        <a
-                          key={page.name}
-                          href={page.href}
-                          className="flex items-center text-sm font-medium text-gray-700 hover:text-gray-800"
-                        >
-                          {page.name}
-                        </a>
-                      ))}
-                    </div>
-                  </Popover.Group>
+                        {navigation.pages.map((page) => (
+                          <a
+                            key={page.name}
+                            href={page.href}
+                            className="flex items-center text-sm font-medium text-gray-700 hover:text-gray-800"
+                          >
+                            {page.name}
+                          </a>
+                        ))}
+                      </div>
+                    </Popover.Group>
+                  </div>
                 </div>
 
                 {/* Mobile menu and search (lg-) */}
