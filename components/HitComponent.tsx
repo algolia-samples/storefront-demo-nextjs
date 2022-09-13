925b58ad5fa57d7ab5e54a90a6460c484127e867
Sarah Dayan <5370675+sarahdayan@users.noreply.github.com>
2022-09-13 19:43:14 +0200

    chore: move link to entire hit

--- components/HitComponent.tsx
+++ components/HitComponent.tsx
@@ -13,29 +13,27 @@ type HitComponentProps = {
 
 export function HitComponent({ hit }: HitComponentProps) {
   return (
-    <div className="group" key={hit.objectID}>
-      <div className="sm:relative aspect-square bg-white group-hover:opacity-75 sm:aspect-none p-6">
-        <Image
-          src={hit.image1}
-          alt={hit.title}
-          className="w-full h-full object-center object-cover sm:w-full sm:h-full"
-          width={512}
-          height={512}
-        />
-      </div>
-      <div className="flex-1 p-4 space-y-2 flex flex-col">
-        <h3 className="mt-4 text-sm text-gray-700 line-clamp-3">
-          <Link href="#">
-            <a>
-              <Highlight hit={hit} attribute="title" />
-            </a>
-          </Link>
-        </h3>
-        <p className="mt-2 text-sm text-gray-500">{hit.brand_label}</p>
-        <p className="mt-2 text-sm font-medium text-gray-900">
-          {formatPrice(hit.price_new, hit.currency)}
-        </p>
-      </div>
-    </div>
+    <Link href={hit.link_grade_v2.href}>
+      <a className="group" key={hit.objectID}>
+        <div className="sm:relative aspect-square bg-white group-hover:opacity-75 sm:aspect-none p-6">
+          <Image
+            src={hit.image1}
+            alt={hit.title}
+            className="w-full h-full object-center object-cover sm:w-full sm:h-full"
+            width={512}
+            height={512}
+          />
+        </div>
+        <div className="flex-1 p-4 space-y-2 flex flex-col">
+          <h3 className="mt-4 text-sm text-gray-700 line-clamp-3">
+            <Highlight hit={hit} attribute="title" />
+          </h3>
+          <p className="mt-2 text-sm text-gray-500">{hit.brand_label}</p>
+          <p className="mt-2 text-sm font-medium text-gray-900">
+            {formatPrice(hit.price_new, hit.currency)}
+          </p>
+        </div>
+      </a>
+    </Link>
   );
 }
