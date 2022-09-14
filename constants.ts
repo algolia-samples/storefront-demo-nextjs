d37331e0b4ea3bcdb6f677704333f20096911545
Sarah Dayan <5370675+sarahdayan@users.noreply.github.com>
2022-09-14 13:32:49 +0200

    feat: remove autocomplete

--- constants.ts
+++
@@ -1,8 +0,0 @@
-export const APP_ID = 'latency';
-export const SEARCH_API_KEY = '1fcf68140f3db8a1352370a137f5e8d6';
-
-export const PRODUCTS_INDEX = 'devcon22_bm_products';
-export const PRODUCTS_PRICE_ASC_INDEX = 'devcon22_bm_products_price_asc';
-export const PRODUCTS_PRICE_DESC_INDEX = 'devcon22_bm_products_price_desc';
-export const PRODUCTS_QUERY_SUGGESTIONS_INDEX =
-  'devcon22_bm_products_query_suggestions';
--- utils/searchClient.ts
+++
@@ -1,5 +0,0 @@
-import algoliasearch from 'algoliasearch';
-
-import { APP_ID, SEARCH_API_KEY } from '../constants';
-
-export const searchClient = algoliasearch(APP_ID, SEARCH_API_KEY);
